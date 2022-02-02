4568a3b2f503c6dd2d82f14aeba20b4e8594049d
akalankaih <akalankaih.19@uom.lk>
2022-02-02 20:54:14 +0530

    test2

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -37,9 +37,7 @@ export default function Footer() {
             <ul className="footer-links">
               <li><a href="http://scanfcode.com/about/">About Us</a></li>
               <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
-              <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>
-              <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
-              <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
+    
             </ul>
           </div>
         </div>
